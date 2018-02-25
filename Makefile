6700aee5345121a67c059e01c2525c9a7cbbf7fb
root <root@ip-172-31-2-106.ec2.internal>
2018-02-25 13:30:07 +0000

    working conf

--- Makefile
+++ Makefile
@@ -12,6 +12,9 @@ start:
 restart:
 	supervisorctl restart jupyterhub
 
+stop:
+	supervisorctl stop jupyterhub
+
 clean-containers:
 	-docker rm -f $(shell docker ps -a | grep 'jupyter-' | awk '{print $$1}')
 
--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -5,17 +5,17 @@ c = get_config()
 # spawn with Docker
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
 c.DockerSpawner.container_image = 'singleuser'
-
+c.DockerSpawner.extra_create_kwargs.update({ 'ports': {'%i/tcp' % 4040: None} })
 # The docker instances need access to the Hub, so the default loopback port doesn't work:
 import netifaces
 docker_ip = netifaces.ifaddresses('docker0')[netifaces.AF_INET][0]['addr']
 c.JupyterHub.hub_ip = docker_ip
+c.DockerSpawner.extra_host_kwargs.update({ 'port_bindings': {'%i/tcp' % 4040: (docker_ip,) }})
 
 #c.JupyterHub.proxy_cmd = ['configurable-http-proxy', '--redirect-port', '80']
 
-
 # OAuth with GitHub
-c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
+#c.JupyterHub.authenticator_class = 'oauthenticator.GitHubOAuthenticator'
 
 c.Authenticator.whitelist = whitelist = set()
 c.Authenticator.admin_users = admin = set()
@@ -36,7 +36,7 @@ with open(join(here, 'userlist')) as f:
         if len(parts) > 1 and parts[1] == 'admin':
             admin.add(name)
 
-c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
+#c.GitHubOAuthenticator.oauth_callback_url = os.environ['OAUTH_CALLBACK_URL']
 
 # ssl config
 ssl = join(here, 'ssl')
